4790573614c0bfd00e811e32e5a01d8a5e10922a
Hawllysson Gardel <hawllysson@rootcodes.com.br>
2021-09-21 16:26:17 -0300

    Remove Email Verify.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,6 @@ class UserFactory extends Factory
         return [
             'name'              => $this->faker->name(),
             'email'             => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token'    => Str::random(10),
             'date_of_birth'     => $this->faker->dateTimeThisCentury->format('Y-m-d'),
@@ -35,19 +34,5 @@ class UserFactory extends Factory
             'address'           => $this->faker->unique()->address,
             'user_type_id'      => UserType::all()->random()->id
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
     }
 }
