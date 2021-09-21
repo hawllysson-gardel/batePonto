78d517b796dacac25274c8b08eacd96d640d1d11
Hawllysson Gardel <hawllysson@rootcodes.com.br>
2021-09-21 18:28:18 -0300

    Validate: Login, Logout, Forgot Password and Reset Password.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,12 +19,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// LOGIN
+Route::get('/', [AuthenticatedSessionController::class, 'create'])->middleware('guest');
+Route::group(['prefix' => 'login', 'middleware' => 'guest'], function () {
+    Route::get('/', [AuthenticatedSessionController::class, 'create']);
+    Route::post('/', [AuthenticatedSessionController::class, 'store'])->name('login');
+});
+
+// LOGOUT
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth')->name('logout');
+
+// FORGOT PASSWORD
+Route::group(['prefix' => 'forgot-password', 'middleware' => 'guest'], function () {
+    Route::get('/', [PasswordResetLinkController::class, 'create'])->name('password.request');
+    Route::post('/', [PasswordResetLinkController::class, 'store'])->name('password.email');
+});
+
+// RESET PASSWORD
+Route::group(['prefix' => 'reset-password', 'middleware' => 'guest'], function () {
+    Route::get('/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
+    Route::post('/', [NewPasswordController::class, 'store'])->name('password.update');
+});
+
+// REGISTER
+Route::group(['prefix' => 'register', 'middleware' => 'guest'], function () {
+    Route::get('/', [RegisteredUserController::class, 'create'])->name('register');
+    Route::post('/', [RegisteredUserController::class, 'store']);
+});
+
+// REGISTER
+Route::group(['prefix' => 'confirm-password', 'middleware' => 'auth'], function () {
+    Route::get('/', [ConfirmablePasswordController::class, 'create'])->name('password.confirm');
+    Route::post('/', [ConfirmablePasswordController::class, 'store']);
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
